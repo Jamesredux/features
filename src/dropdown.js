46ffcf955fca07c3b4c7c81ca6a24be2b1586023
James McCann <jamesredux@gmail.com>
2020-08-30 21:19:07 +0100

    dropdown function added

--- src/dropdown.js
+++ src/dropdown.js
@@ -2,11 +2,23 @@ const { doc } = require("prettier");
 
 const dropdown = (() => {
   const toggleDropdown = (e) => {
-    console.log(e);
+    const dropdownItems = e.target.nextElementSibling;
+    dropdownItems.classList.toggle("hidden");
+  };
+
+  const menuPick = (e) => {
+    const selection = e.target.dataset;
+    console.log(e.target.dataset.type);
+    console.log(selection);
   };
   const dropdownButtons = document.querySelectorAll(".dropdown-btn");
+  const menuChoices = document.querySelectorAll(".menu-item");
+
   dropdownButtons.forEach((button) =>
     button.addEventListener("click", toggleDropdown, false)
+  );
+  menuChoices.forEach((choice) =>
+    choice.addEventListener("click", menuPick, false)
   );
 })();
 
