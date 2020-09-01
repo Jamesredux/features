ff2f26c24ca516f85ad4167198e3b84314c885ed
James McCann <jamesredux@gmail.com>
2020-09-01 12:34:07 +0100

    complete manual slideshow

--- src/slide.js
+++ src/slide.js
@@ -1,14 +1,53 @@
 const { doc } = require("prettier");
 
 const slide = (() => {
-  const selectSlide = (e) => {
-    console.log(e.target.dataset.slide);
-  };
   const slideBullets = document.querySelectorAll(".bullet");
+  const slidesRow = document.querySelector(".slides-row");
+  const backArrow = document.querySelector(".back");
+  const forwardArrow = document.querySelector(".forward");
+  let currentSlide = 1;
+
+  const moveBack = () => {
+    if (currentSlide === 1) return;
+    currentSlide = currentSlide - 1;
+
+    changeImage(currentSlide);
+  };
+
+  const moveForward = () => {
+    if (currentSlide === 6) return;
+    currentSlide = currentSlide + 1;
+    changeImage(currentSlide);
+  };
+
+  const changeImage = (slideNumber) => {
+    changeSelectedBullet(slideNumber);
+    showPicture(slideNumber);
+  };
+
+  const changeSelectedBullet = (bulletNumber) => {
+    slideBullets.forEach((bullet) => bullet.classList.remove("selected"));
+    const selectedBullet = document.querySelector(
+      `[data-slide="${bulletNumber}"]`
+    );
+    selectedBullet.classList.add("selected");
+  };
+
+  const showPicture = (bulletNumber) => {
+    const transformValue = bulletNumber * 550 - 550;
+    slidesRow.style.transform = `translateX(-${transformValue}px)`;
+  };
+  const selectSlide = (e) => {
+    const bulletNumber = e.target.dataset.slide;
+    currentSlide = parseInt(bulletNumber);
+    changeImage(currentSlide);
+  };
 
   slideBullets.forEach((bullet) =>
     bullet.addEventListener("click", selectSlide)
   );
+  forwardArrow.addEventListener("click", moveForward);
+  backArrow.addEventListener("click", moveBack);
 })();
 
 export default slide;
